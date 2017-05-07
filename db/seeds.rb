c94930ac86051d3daed54b4c1866c73fed5e3a5e
User <mmadov@ubuntu.ubuntu-domain>
2017-05-08 01:12:27 +0400

    Seed nokogiri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
+doc.xpath('//table//tbody//tr[position() > 1]').each do |link|
+  puts Card.create(original_text: link.xpath("td[2]").text,translated_text:link.xpath("td[3]").text)
+end
