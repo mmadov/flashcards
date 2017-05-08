155c10c6c3275076261f4a1d3daaa77ba8bb7a27
User <mmadov@ubuntu.ubuntu-domain>
2017-05-08 22:44:09 +0400

    5 lesson

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
     def index
+        @card = Card.review_date
     end
+    def sub
+               @cards = Card.find(params[:id])
+               if @cards.translated_text === params[:translated_text]
+               @cards = Card.find(params[:id])
+               @tess = "Правильно"
+               @cards.update_review_date
+        else
+               @tess = "Не правильно"
+        end
+    end
+   
+     
+
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -8,7 +8,13 @@ end
 class Card < ApplicationRecord
 before_save :normalize_card_review_date
 validates_with RecordValidator
-
+scope :review_date, -> {
+  where('review_date > ?', Time.now)
+  order("RANDOM()").first
+}
+  def update_review_date
+   update(review_date: Time.now+ 3.days)
+  end 
   def normalize_card_review_date
       self.review_date = Time.now+ 3.days
   end
