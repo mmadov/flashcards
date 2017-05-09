2f4a3f9079e68f920dd12bd7466814be04b4c054
User <mmadov@ubuntu.ubuntu-domain>
2017-05-10 01:07:36 +0400

    Dorabotka

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,12 +6,16 @@ class RecordValidator < ActiveModel::Validator
   end
 end
 class Card < ApplicationRecord
+  
   before_create :normalize_card_review_date
   validates_with RecordValidator
   scope :review_date, -> {
     where('review_date < ?', Time.now)
     order("RANDOM()").first
   }
+def check_translation(user_input) 
+  translated_text === user_input 
+end
   def update_review_date
     update!(review_date: Time.now+3.days)
   end 
