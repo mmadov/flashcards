0e91eec8d70e5894c1a1a7576e0ec6e945c4b9bd
User <mmadov@ubuntu.ubuntu-domain>
2017-05-10 01:42:07 +0400

    Dorabotka3

--- app/models/card.rb
+++ app/models/card.rb
@@ -12,13 +12,17 @@ class Card < ApplicationRecord
     where('review_date < ?', Time.now)
     order('RANDOM()').first
   }
+
   def check_translation(user_input) 
     translated_text === user_input 
   end
+
   def update_review_date
     normalize_card_review_date && save
   end 
+
   def normalize_card_review_date
     self.review_date = Time.now + 3.days
   end
+  
 end
