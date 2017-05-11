f6f9bf04431fb2df25004a78230600869d62553a
User <mmadov@ubuntu.ubuntu-domain>
2017-05-11 14:54:10 +0400

    Rspec1

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,8 +3,7 @@ require "rails_helper"
 RSpec.describe Card, :type => :model do
   it "orders by last name" do
     time = Time.now + 3.days
-    card = Card.create!(original_text: "durch",translated_text: "by")
-    expect(card.review_date.strftime("%B %d, %Y")).to eq(time.strftime("%B %d, %Y"))
+    card = Card.create!(original_text: 'durch',translated_text: 'by')
+    expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
   end
-
 end
