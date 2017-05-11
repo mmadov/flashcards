c0d82d482fa1e4898172f24eec76279ba3150f8c
User <mmadov@ubuntu.ubuntu-domain>
2017-05-11 16:51:06 +0400

    Rspec6

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Card, :type => :model do
-  it 'Тест на время' do
+  it 'Date value' do
     time = Time.now + 3.days
     card = Card.create!(original_text: 'durch',translated_text: 'test')
     expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
   end
-  it 'С пробелом значения' do
+  it 'False value' do
     time = Time.now + 3.days
     card = Card.create!(original_text: 'cat', translated_text: 'кот')
     expect(card.check_translation('собака')).to be false
   end
-  it 'Разные значение' do
+  it 'True value' do
     time = Time.now + 3.days
     card = Card.create!(original_text: 'cat', translated_text: 'кот')
     expect(card.check_translation('кот')).to be true
