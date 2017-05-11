35996c12e8dbb0aa523d603fed5781cb6471eac8
User <mmadov@ubuntu.ubuntu-domain>
2017-05-11 16:35:39 +0400

    Rspec5

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,18 +2,18 @@ require "rails_helper"
 
 RSpec.describe Card, :type => :model do
   it 'Тест на время' do
-      time = Time.now + 3.days
-      card = Card.create!(original_text: 'durch',translated_text: 'test')
-      expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
+    time = Time.now + 3.days
+    card = Card.create!(original_text: 'durch',translated_text: 'test')
+    expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
   end
-    it 'С пробелом значения' do
-      time = Time.now + 3.days
-      card = Card.create!(original_text: 'cat', translated_text: 'кот')
-      expect(card.check_translation('собака')).to be false
+  it 'С пробелом значения' do
+    time = Time.now + 3.days
+    card = Card.create!(original_text: 'cat', translated_text: 'кот')
+    expect(card.check_translation('собака')).to be false
   end
-    it 'Разные значение' do
-      time = Time.now + 3.days
-      card = Card.create!(original_text: 'cat', translated_text: 'кот')
-      expect(card.check_translation('кот')).to be true
+  it 'Разные значение' do
+    time = Time.now + 3.days
+    card = Card.create!(original_text: 'cat', translated_text: 'кот')
+    expect(card.check_translation('кот')).to be true
   end
 end
