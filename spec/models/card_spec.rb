42318e5e58b2fea0e37159c6a3f464372a123eef
User <mmadov@ubuntu.ubuntu-domain>
2017-05-11 17:21:55 +0400

    Rspec8

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -7,12 +7,10 @@ RSpec.describe Card, :type => :model do
     expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
   end
   it 'Wrong data input' do
-    time = Time.now + 3.days
     card = Card.create!(original_text: 'cat', translated_text: 'кот')
     expect(card.check_translation('собака')).to be false
   end
   it 'True data input' do
-    time = Time.now + 3.days
     card = Card.create!(original_text: 'cat', translated_text: 'кот')
     expect(card.check_translation('кот')).to be true
   end
