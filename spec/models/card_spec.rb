5780436c247510d81368ad102fab13013c1144d8
User <mmadov@ubuntu.ubuntu-domain>
2017-05-11 15:47:03 +0400

    Rspec2

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Card, :type => :model do
-  it "orders by last name" do
-    time = Time.now + 3.days
-    card = Card.create!(original_text: 'durch',translated_text: 'by')
-    expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
+  it 'Равные значения' do
+      time = Time.now + 3.days
+      card = Card.create!(original_text: 'durch',translated_text: 'durch')
+      expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
+  end
+    it 'С пробелом значения' do
+      time = Time.now + 3.days
+      card = Card.create!(original_text: 'durch',translated_text: ' durcH')
+      expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
+  end
+    it 'Разные значение' do
+      time = Time.now + 3.days
+      card = Card.create!(original_text: 'durch',translated_text: 'by')
+      expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
   end
 end
