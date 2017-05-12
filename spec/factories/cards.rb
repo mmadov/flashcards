b5f89507e33fc4385bb2aaff0e9d317a29b642b2
User <mmadov@ubuntu.ubuntu-domain>
2017-05-13 01:16:55 +0400

    Factrory 1

---
+++ spec/factories/cards.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :card do
+    original_text "cat" 
+    translated_text "кот"
+  end
+end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,17 +1,17 @@
 require "rails_helper"
 
-RSpec.describe Card, :type => :model do
+RSpec.describe Card do
   it 'Time Test' do
     time = Time.now + 3.days
-    card = Card.create!(original_text: 'durch',translated_text: 'test')
+    card = FactoryGirl.create(:card)
     expect(card.review_date.strftime('%B %d, %Y')).to eq(time.strftime('%B %d, %Y'))
   end
   it 'Wrong data input' do
-    card = Card.create!(original_text: 'cat', translated_text: 'кот')
+    card = FactoryGirl.create(:card)
     expect(card.check_translation('собака')).to be false
   end
   it 'True data input' do
-    card = Card.create!(original_text: 'cat', translated_text: 'кот')
+    card = FactoryGirl.create(:card)
     expect(card.check_translation('кот')).to be true
   end
 end
