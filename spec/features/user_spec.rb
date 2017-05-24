a96413d7075623684b4e079df45e36719ac0ee1e
User <mmadov@ubuntu.ubuntu-domain>
2017-05-24 14:54:07 +0400

    Sorcery 3

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -8,17 +8,15 @@ describe 'User login' do
   it 'Login successful' do
   fill_in :email, with: user.email
   fill_in :password, with: "mamedov"
-    save_and_open_page
+  save_and_open_page
   click_button 'Login'
-
   expect(page).to have_content 'Login successful'
   end
   it 'Login failed' do
   fill_in :email, with: 'fake'
   fill_in :password, with: 'fake'  
-    save_and_open_page
+  save_and_open_page
   click_button 'Login'
-
   expect(page).to have_content 'Login failed'
   end
 end
